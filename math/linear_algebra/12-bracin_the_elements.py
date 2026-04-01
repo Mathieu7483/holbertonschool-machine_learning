f8526c8253ba50dee56d45ebb1fa2bcff03c30d8
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-01 12:39:19 +0200

    Add np_elementwise function for element-wise operations on matrices

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""file that contains the multiple element-wise operations"""
+
+
+def np_elementwise(mat1, mat2):
+    """function that performs element-wise addition, subtraction,
+    multiplication, and division
+    Returns:
+        the element-wise addition, subtraction, multiplication,
+        and division of the two matrices as a tuple
+    """
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
+    return add, sub, mul, div
