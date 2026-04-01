0751de216135a46606ffd20aa4ab44e847e1ed63
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-01 12:31:54 +0200

    Remove unnecessary argument documentation from np_shape function

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python3
 """file that contains the function np_shape"""
 
-
-import numpy as np
-
-
 def np_shape(matrix):
     """function that calculates the shape of a matrix
-
-    Args:
-        matrix: is a numpy.ndarray of any shape
-
     Returns:
         the shape of the matrix as a tuple
     """
