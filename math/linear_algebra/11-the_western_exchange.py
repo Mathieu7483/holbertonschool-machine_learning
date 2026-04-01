b942edb0101cf608ffde362b9201fe7e744a7009
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-01 12:35:34 +0200

    Add np_transpose function to transpose a matrix

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" file that contains the function np_transpose"""
+
+
+def np_transpose(matrix):
+    """function that transposes a matrix
+    Returns:
+        the transposed matrix
+    """
+    return matrix.T
