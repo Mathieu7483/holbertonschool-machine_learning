2cb15427718344da49f58e94906e2aef13fd5d3f
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-03 13:05:40 +0200

    Remove unnecessary whitespace in cat_matrices function for improved readability

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -13,14 +13,14 @@ def cat_matrices(mat1, mat2, axis=0):
         return mat1 + mat2
     if not isinstance(mat1, list) or not isinstance(mat2, list):
         return None
-    
+
     if len(mat1) != len(mat2):
         return None
-    
+
     result = []
     for i in range(len(mat1)):
         sub_cat = cat_matrices(mat1[i], mat2[i], axis - 1)
-        
+
         if sub_cat is None:
             return None
         result.append(sub_cat)
