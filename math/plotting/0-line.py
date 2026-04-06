f987992b45fd558715cf77e49a9afdb51c92b26c
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-06 10:08:40 +0200

    Add line plotting functionality with cubic equation visualization

---
+++ math/plotting/0-line.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+def line():
+
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.plot(y)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('y = x^3')
+    plt.xlim(0, 10)
+    plt.ylim(0, 1000)
+    plt.grid()
+    plt.show()  
