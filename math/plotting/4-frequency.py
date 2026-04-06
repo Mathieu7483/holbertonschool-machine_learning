b05846c809454d20be8919dd75defa540c144842
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-06 13:04:37 +0200

    Add frequency function to plot histogram of student grades

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module for plotting a histogram of student grades."""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """Plots a histogram of student grades with 10 bins."""
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.hist(student_grades, bins=10, edgecolor='black')
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+    plt.grid()(axis='y', alpha=0.75)
+    plt.show()
