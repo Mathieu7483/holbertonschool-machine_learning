e154fe4d8935a6e04d584cd6622afbc6b0a32489
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-06 10:41:30 +0200

    Refactor line function to use 'r-' for plotting and remove unnecessary color specification

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python3
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-
 def line():
+    """
+    Plots y = x^3 as a red line
+    """
     x = np.arange(0, 11)
     y = x ** 3
+
+    # On définit la figure AVANT le plot
     plt.figure(figsize=(6.4, 4.8))
-    plt.plot(x, y, color='red')
+
+    # On passe explicitement x et y, avec le formatage 'r-'
+    plt.plot(x, y, 'r-')
+
+    # Configuration des labels et limites
     plt.xlabel('x')
     plt.ylabel('y')
     plt.title('y = x^3')
+    
+    # On s'assure que les limites sont bien des entiers ou flottants exacts
     plt.xlim(0, 10)
     plt.ylim(0, 1000)
-    plt.savefig('line.png')
-    plt.close()
 
-
-line()
+    plt.show()
