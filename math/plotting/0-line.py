0ff2cead785bc6926a48b773002784e63d5b5df1
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-06 10:46:18 +0200

    Refactor line function to streamline code and remove unnecessary comments

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,26 +2,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def line():
-    # Définition explicite de X et Y
-    x = np.arange(0, 11)
-    y = x ** 3
 
-    # Initialisation de la figure
+def line():
+    y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
-    # Tracer AVEC x et y explicitement, en rouge ('r')
-    # L'argument '-' assure que c'est une ligne continue
+    x = np.arange(0, 11)
     plt.plot(x, y, 'r-')
-
-    # Labels et Titre (Vérifie bien qu'il n'y a pas d'espace en trop)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('y = x^3')
-
-    # Limites strictes
     plt.xlim(0, 10)
-    plt.ylim(0, 1000)
-
-    # Affichage
     plt.show()
