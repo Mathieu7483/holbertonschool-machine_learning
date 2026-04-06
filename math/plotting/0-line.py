925aea80706d0008bd3cf794b8fe9db33d3eb81e
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-06 10:20:23 +0200

    Fix x-axis data in 0-line.py to correctly plot cubic function

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -4,14 +4,17 @@ import matplotlib.pyplot as plt
 
 
 def line():
-
+    x = np.arange(0, 11)
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.plot(y, 'r-')
+    plt.plot(x, y, 'r-')
+
     plt.xlabel('x')
     plt.ylabel('y')
     plt.title('y = x^3')
+    
     plt.xlim(0, 10)
     plt.ylim(0, 1000)
+
     plt.show()
