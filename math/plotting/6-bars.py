a559b3dd5bd18c0e14f8fc72692c7a48cb1ec0da
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-06 18:40:46 +0200

    Refactor bar chart plotting for improved readability and consistency

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -8,10 +8,11 @@ def bars():
     """Plots a stacked bar chart of fruit quantities for three people."""
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4, 3))
+    
     plt.figure(figsize=(6.4, 4.8))
 
     names = ['Farrah', 'Fred', 'Felicia']
-    fruits = ['apples', 'bananas', 'oranges', 'peaches' ]
+    fruits = ['apples', 'bananas', 'oranges', 'peaches']
     colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
 
     indices = np.arange(len(names))
@@ -19,12 +20,16 @@ def bars():
     bottom = np.zeros(len(names))
 
     for i in range(len(fruits)):
-        plt.bar(indices, fruit[i], bottom=bottom, color=colors[i],
-                label=fruits[i])
+        plt.bar(indices, fruit[i], width=width, bottom=bottom, 
+                color=colors[i], label=fruits[i])
         bottom += fruit[i]
+
     plt.ylabel('Quantity of Fruit')
     plt.title('Number of Fruit per Person')
+
     plt.xticks(indices, names)
-    plt.yticks(range(0, 81, 10))
-    plt.legend(loc='upper right')
+    plt.yticks(np.arange(0, 81, 10))
+    plt.ylim(0, 80)
+    
+    plt.legend()
     plt.show()
