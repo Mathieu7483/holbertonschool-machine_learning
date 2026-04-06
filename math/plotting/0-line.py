80cd5fd4a8c1725f81c63e9944161f877462f328
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-06 10:47:12 +0200

    Enhance module docstring and function comment for clarity

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""Module for plotting a cubic line graph."""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+    """Plots y = x^3 as a solid red line with x-axis from 0 to 10."""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
