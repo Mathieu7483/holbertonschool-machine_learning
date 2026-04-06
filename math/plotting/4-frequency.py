72453d39e79f65320a2d74d2e1a72e79097a0974
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-06 13:11:12 +0200

    Refactor frequency function docstring and remove unnecessary plot elements

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -5,18 +5,14 @@ import matplotlib.pyplot as plt
 
 
 def frequency():
-    """Plots a histogram of student grades with 10 bins."""
+    """Plots a histogram of student scores for Project A."""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
-    
     plt.figure(figsize=(6.4, 4.8))
 
     plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
-    plt.xticks(np.arange(0, 101, 10))
     plt.xlim(0, 100)
-    plt.ylim(0, 30)
-    plt.grid(axis='y', alpha=0.75)
     plt.show()
