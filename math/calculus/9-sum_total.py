2169e96c4b212be1065f95323af965e2b82ab5b4
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-07 11:18:09 +0200

    Fix type check condition in summation_i_squared function

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,6 +5,6 @@
 def summation_i_squared(n):
     """Calculates the sum of i^2 for i from 1 to n.
     n is the stopping condition"""
-    if n is not isinstance(n, int) or n < 1:
+    if not isinstance(n, int) or n < 1:
         return None
     return n * (n + 1) * (2 * n + 1) // 6
