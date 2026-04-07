bfe3d3484f4b3d8d3c035558a3cf312834937a6c
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-07 11:47:57 +0200

    Add poly_derivative function to calculate polynomial derivatives

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""function that calculates the derivative of a polynomial."""
+
+
+def poly_derivative(poly):
+    """Calculates the derivative of a polynomial.
+    poly is a list of coefficients representing the polynomial
+    Returns a new list of coefficients representing the derivative
+    """
+    if not isinstance(poly, list):
+        return None
+    if not all(isinstance(coef, int) for coef in poly):
+        return None
+    if poly [1:] == [0]:
+        return [0]
+    return [coef * i for i, coef in enumerate(poly)][1:]
