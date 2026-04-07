714b13544b840a82601eff795f4a6a14216b01a9
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-07 11:14:32 +0200

    Remove unnecessary check for n < 1 in summation_i_squared function

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -7,6 +7,4 @@ def summation_i_squared(n):
     n is the stopping condition"""
     if n is None or not isinstance(n, int):
         return None
-    if n < 1:
-        return 0
     return n * (n + 1) * (2 * n + 1) // 6
