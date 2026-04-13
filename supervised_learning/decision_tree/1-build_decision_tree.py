6c45e55fbc43c14e06518a72e2409328a1e8e841
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-13 10:20:29 +0200

    Implement decision tree classes with node and leaf structures, including methods for depth calculation and node counting

---
+++ supervised_learning/decision_tree/1-build_decision_tree.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""0. Build a decision tree"""
+
+import numpy as np
+
+
+class Node:
+    """Class Node that represents a node in a decision tree"""
+    def __init__(self, feature=None, threshold=None,
+                 left_child=None, right_child=None, is_root=False, depth=0):
+        self.feature = feature
+        self.threshold = threshold
+        self.left_child = left_child
+        self.right_child = right_child
+        self.is_leaf = False
+        self.is_root = is_root
+        self.sub_population = None
+        self.depth = depth
+
+    def max_depth_below(self):
+        """Method that returns the maximum depth of the tree below the node"""
+        if self.is_leaf:
+            return self.depth
+        else:
+            return max(self.left_child.max_depth_below(),
+                       self.right_child.max_depth_below())
+
+
+class Leaf(Node):
+    """Class Leaf that represents a leaf in a decision tree"""
+    def __init__(self, value, depth=None):
+        super().__init__()
+        self.value = value
+        self.is_leaf = True
+        self.depth = depth
+
+    def max_depth_below(self):
+        """Method that returns the maximum depth of the tree below the leaf"""
+        return self.depth
+
+
+class Decision_Tree():
+    """Class Decision_Tree that represents a decision tree"""
+    def __init__(self, max_depth=10, min_pop=1,
+                 seed=0, split_criterion="random", root=None):
+        self.rng = np.random.default_rng(seed)
+        if root:
+            self.root = root
+        else:
+            self.root = Node(is_root=True)
+        self.explanatory = None
+        self.target = None
+        self.max_depth = max_depth
+        self.min_pop = min_pop
+        self.split_criterion = split_criterion
+        self.predict = None
+
+    def depth(self):
+        """Method that returns the maximum depth of the tree"""
+        return self.root.max_depth_below()
+
+
+def count_nodes(self, only_leaves=False):
+    """Method that returns the number of nodes in the tree"""
+    return self.root.count_nodes_below(only_leaves=only_leaves)
+
+
+def count_nodes_below(self, only_leaves=False):
+    """Method that returns the number of nodes in the tree below the node"""
+    return 1
