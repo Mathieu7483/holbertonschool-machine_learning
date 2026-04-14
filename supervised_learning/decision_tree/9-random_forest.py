26c99afc260cfc8ba92781c29b925ac6a985b65b
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-14 15:16:54 +0200

    Implement Random Forest Classifier with fitting and prediction methods

---
+++ supervised_learning/decision_tree/9-random_forest.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""Random Forest Classifier"""
+
+import numpy as np
+Decision_Tree = __import__('8-build_decision_tree').Decision_Tree
+
+
+class Random_Forest:
+    """Random Forest Classifier"""
+
+    def __init__(self, n_trees=100, max_depth=10, min_pop=1, seed=0):
+        self.numpy_predicts = []
+        self.target = None
+        self.numpy_preds = None
+        self.n_trees = n_trees
+        self.max_depth = max_depth
+        self.min_pop = min_pop
+        self.seed = seed
+
+    def predict(self, explanatory):
+        """Predict the class labels for the input data"""
+        tree_predictions = np.array([tree.predict(explanatory)
+                                     for tree in self.trees])
+        # Majority vote
+        return np.apply_along_axis(lambda x: np.bincount(x).argmax(),
+                                   axis=0, arr=tree_predictions)
+
+    def fit(self, explanatory, target, n_trees=100, verbose=0):
+        self.target = target
+        self.explanatory = explanatory
+        self.trees = []
+        depths = []
+        nodes = []
+        leaves = []
+        accuracies = []
+        for i in range(n_trees):
+            T = Decision_Tree(max_depth=self.max_depth,
+                              min_pop=self.min_pop,
+                              seed=self.seed + i)
+            T.fit(explanatory, target)
+            self.trees.append(T)
+            depths.append(T.depth())
+            nodes.append(T.count_nodes())
+            leaves.append(T.count_nodes(only_leaves=True))
+            accuracies.append(T.accuracy(T.explanatory, T.target))
+        if verbose == 1:
+            print(f"""  Training finished.
+    - Mean depth                     : {np.array(depths).mean()}
+    - Mean number of nodes           : {np.array(nodes).mean()}
+    - Mean number of leaves          : {np.array(leaves).mean()}
+    - Mean accuracy on training data : {np.array(accuracies).mean()}
+    - Accuracy of the forest on td   : {self.accuracy(self.explanatory, self.target)}""")
+
+    def __str__(self):
+        """Visual representation of the forest"""
+        return '\n'.join([f'Tree {i}:\n{str(tree)}'
+                          for i, tree in enumerate(self.trees)])
