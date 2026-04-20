8b505a288fd549d588e42517ff8e53ca310d9f8f
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-20 12:37:24 +0200

    Update Neuron class docstring and improve constructor method comments

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Defines a single neuron performing binary classification"""
+"""Write a class Neuron that defines a single neuron performing
+binary classification (Based on 0-neuron.py):"""
 
 import numpy as np
 
@@ -9,15 +10,33 @@ class Neuron:
     performing binary classification"""
 
     def __init__(self, nx):
-        """Constructor method
+        """Constructor method for the class Neuron
 
         Args:
-            nx: number of input features to the neuron
+            nx: is the number of input features to the neuron
         """
-        if type(nx) is not int:
+        if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         self.W = np.random.normal(size=(1, nx))
         self.b = 0
         self.A = 0
+
+
+@property
+def A(self):
+    """The getter method for the output of the neuron (prediction)"""
+    return self.__A
+
+
+@property
+def W(self):
+    """The getter method for the weights vector of the neuron"""
+    return self.__W
+
+
+@property
+def b(self):
+    """The getter method for the bias of the neuron"""
+    return self.__b
