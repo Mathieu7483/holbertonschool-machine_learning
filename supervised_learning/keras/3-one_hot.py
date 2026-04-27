ffbc505f63316c81b0d2a87877834d673b16c8e2
Mathieu7483 <mathieu.godalier@orange.fr>
2026-04-27 09:40:22 +0200

    Add one_hot function to convert numeric labels into a one-hot matrix

---
+++ supervised_learning/keras/3-one_hot.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""converts a numeric label vector into a one-hot matrix"""
+
+from tensorflow import keras as K
+
+def one_hot(labels, classes=None):
+  """converts a numeric label vector into a one-hot matrix"""
+  return K.utils.to_categorical(labels, num_classes=classes)
