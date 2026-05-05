bc815d4cc1f559ef97a4696e4aaa579194529115
Mathieu7483 <mathieu.godalier@orange.fr>
2026-05-05 19:37:19 +0200

    Refactor create_batch_norm_layer function to improve documentation and structure

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
-"""Write the function def create_batch_norm_layer(prev, n, activation):
-that creates a batch normalization layer for
-a neural network in tensorflow:"""
+"""Module for creating a batch normalization layer in TensorFlow."""
 import tensorflow as tf
 
 
 def create_batch_norm_layer(prev, n, activation):
-    """Creates a batch normalization layer for a neural network in tensorflow.
+    """
+    Creates a batch normalization layer for a neural network in TensorFlow.
 
-    Args:
-        prev: The output from the previous layer.
-        n: The number of nodes in the layer to be created.
-        activation: The activation function that should be used on the
-        output of the layer.
+    Parameters:
+    prev (tensor): The activated output of the previous layer.
+    n (int): The number of nodes in the layer to be created.
+    activation (function): The activation function to be used on the output
+        of the layer.
 
     Returns:
-        The activated output of the batch normalization layer."""
-    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-    dense = tf.keras.layers.Dense(
-        units=n,
-        kernel_initializer=initializer,
-        use_bias=False
-    )(prev)
-    batch_norm = tf.keras.layers.BatchNormalization()(dense)
-    return activation(batch_norm)
+    tensor: The activated output for the layer.
+    """
+    init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+
+    # Create a dense layer
+    dense = tf.keras.layers.Dense(units=n, kernel_initializer=init)
+    Z = dense(prev)
+
+    # Create a batch normalization layer
+    gamma = tf.Variable(tf.ones([n]), trainable=True)
+    beta = tf.Variable(tf.zeros([n]), trainable=True)
+    mean, variance = tf.nn.moments(Z, axes=0)
+    Z_norm = tf.nn.batch_normalization(Z, mean, variance, beta, gamma, 1e-7)
+
+    # Apply the activation function
+    return activation(Z_norm)
