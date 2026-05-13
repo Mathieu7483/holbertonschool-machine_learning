b7f63503df449090669311c047b52f7908f34e0d
Mathieu7483 <mathieu.godalier@orange.fr>
2026-05-13 16:03:54 +0200

    Refactor dropout_forward_prop function for clarity and consistency in documentation

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -1,35 +1,43 @@
 #!/usr/bin/env python3
-"""Write a function that conducts forward propagation using Dropout:"""
+"""
+This module contains a function that conducts propagation using
+Dropout
+"""
 import numpy as np
 
 
 def dropout_forward_prop(X, weights, L, keep_prob):
-    """conducts forward propagation using Dropout:
-    X is the input data for the network
-    weights is a dictionary of the weights and biases of the neural network
-    L is the number of layers in the network
-    keep_prob is the probability that a node will be kept
-    Returns: a dictionary containing the outputs of each layer and the dropout
-        masks used on each layer, respectively"""
-    cache = {}
-    A = X
+    """
+    Conducts forward propagation using Dropout
+    Parameters:
+        X (numpy.ndarray): Input data for the network, shape (nx, m)
+        weights (dict): Dictionary of the weights and biases.
+        L (int): Number of layers in the network
+        keep_prob (float): Probability that a node will be kept
+    Returns:
+        dict: Outputs of each layer and the dropout mask used on each layer
+    """
+    cache = {'A0': X}
+    for layer in range(1, L + 1):
+        W = weights['W' + str(layer)]
+        b = weights['b' + str(layer)]
+        A_prev = cache['A' + str(layer - 1)]
 
-    for i in range(1, L + 1):
-        W = weights['W' + str(i)]
-        b = weights['b' + str(i)]
-        Z = np.matmul(W, A) + b
+        Z = np.dot(W, A_prev) + b
 
-        if i == L:
-            A = np.exp(Z) / np.sum(np.exp(Z), axis=0, keepdims=True)
+        if layer == L:
+            # Softmax activation for the last layer
+            exp_Z = np.exp(Z - np.max(Z, axis=0, keepdims=True))
+            A = exp_Z / np.sum(exp_Z, axis=0, keepdims=True)
         else:
+            # Tanh activation for all other layers
             A = np.tanh(Z)
 
-            D = np.random.rand(A.shape[0], A.shape[1]) < keep_prob
-            A *= D
-            A /= keep_prob
+            # Apply dropout mask
+            D = (np.random.rand(*A.shape) < keep_prob).astype(int)
+            A = np.multiply(A, D) / keep_prob
+            cache['D' + str(layer)] = D
 
-            cache['D' + str(i)] = D
-
-        cache['A' + str(i)] = A
+        cache['A' + str(layer)] = A
 
     return cache
