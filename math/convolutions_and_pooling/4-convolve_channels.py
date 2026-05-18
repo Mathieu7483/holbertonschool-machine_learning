9ef4c4de9af9b9140873c3049e3abeafe6efd24e
Mathieu7483 <mathieu.godalier@orange.fr>
2026-05-18 15:53:42 +0200

    Add stride parameter to convolve_channels function for flexible convolution

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 
-def convolve_channels(images, kernel, padding='same'):
+def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     """performs a convolution on images with channels
 
     Args:
@@ -24,15 +24,16 @@ def convolve_channels(images, kernel, padding='same'):
             if a tuple:
                 ph: padding for the height of the image
                 pw: padding for the width of the image
+        stride: tuple of (sh, sw)
+            sh: stride for the height of the image
+            sw: stride for the width of the image
     Returns: numpy.ndarray containing the convolved images"""
     m, h, w, c = images.shape
     kh, kw, kc = kernel.shape
-    if c != kc:
-        raise ValueError("The number of channels in the kernel must match "
-                         "the number of channels in the images")
+    sh, sw = stride
     if padding == 'same':
-        ph = (kh - 1) // 2
-        pw = (kw - 1) // 2
+        ph = ((h - 1) * sh + kh - h) // 2 + 1
+        pw = ((w - 1) * sw + kw - w) // 2 + 1
     elif padding == 'valid':
         ph = 0
         pw = 0
@@ -40,12 +41,12 @@ def convolve_channels(images, kernel, padding='same'):
         ph, pw = padding
     padded_images = np.pad(
         images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant')
-    conv_h = h + 2 * ph - kh + 1
-    conv_w = w + 2 * pw - kw + 1
+    conv_h = (h + 2 * ph - kh) // sh + 1
+    conv_w = (w + 2 * pw - kw) // sw + 1
     convolved_images = np.zeros((m, conv_h, conv_w))
     for i in range(conv_h):
         for j in range(conv_w):
             convolved_images[:, i, j] = np.sum(
-                padded_images[:, i:i + kh, j:j + kw] * kernel,
-                axis=(1, 2, 3))
+                padded_images[:, i * sh:i * sh + kh, j * sw:j * sw + kw] *
+                kernel, axis=(1, 2, 3))
     return convolved_images
