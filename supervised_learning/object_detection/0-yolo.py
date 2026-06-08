bbe362d597fa62d61815b39692e4f45111bb8c04
Mathieu7483 <mathieu.godalier@orange.fr>
2026-06-08 12:22:56 +0200

    Refactor docstring in Yolo class constructor for clarity and consistency

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -8,28 +8,23 @@ class Yolo:
     """Class Yolo to perform object detection using YOLOv3"""
 
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-        """Class constructor
-        model_path is the path to where a Darknet Keras model is stored
-        classes_path is the path to where the list of class names used for
-        the Darknet model, listed in order of index, can be found
-        class_t is a float representing the box score threshold for
-        the initial filtering step nms_t is a float representing the
-        IOU threshold for non-max suppression
-        anchors is a numpy.ndarray of shape (outputs, anchor_boxes, 2)
-        containing the anchor boxes:
-        outputs is the number of outputs (predicts) made by the Darknet model
-        anchor_boxes is the number of anchor boxes used for each prediction
-        2 => [anchor_box_width, anchor_box_height]
-        Sets the public instance attributes:
-        model: the Darknet Keras model
-        class_names: a list of the class names for the model
-        class_t: the box score threshold for the initial filtering step
-        nms_t: the IOU threshold for non-max suppression
-        anchors: the anchor boxes
+        """
+        Initializes the Yolo instance.
+
+        Parameters:
+        - model_path: path to where a Darknet Keras model is stored
+        - classes_path: path to where the list of class names used for
+            the Darknet model is found
+        - class_t: float representing the box score threshold for the
+            initial filtering step
+        - nms_t: float representing the IOU threshold for non-max
+            suppression
+        - anchors: numpy.ndarray of shape (outputs, anchor_boxes, 2)
+            containing all of the anchor boxes
         """
         self.model = K.models.load_model(model_path)
-        with open(classes_path, 'r') as f:
-            self.class_names = [line.strip() for line in f]
+        with open(classes_path, 'r') as file:
+            self.class_names = file.read().strip().split('\n')
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
