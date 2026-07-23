Fix PDF function to check for zero determinant in covariance matrix

unsupervised_learning/clustering/5-pdf.py
@@ -34,7 +34,7 @@ def pdf(X, m, S):
 
     # Determinant and inverse of the covariance matrix
     det_S = np.linalg.det(S)
-    if det_S <= 0:
+    if det_S == 0:
         # Determinant is zero, can't invert, stopping here
         return None
 