7e90c8ca253a50dc28297b7f64e09258504d3d89
Mathieu7483 <mathieu.godalier@orange.fr>
2026-07-23 10:46:53 +0200

    Enhance BIC function to validate EM algorithm results and improve parameter calculation

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -73,10 +73,11 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
         pi, m, S, g, li = expectation_maximization(
             X, k, iterations, tol, verbose)
 
-        if pi is None or m is None or S is None or g is None:
+        # verify that the EM algorithm returned valid results
+        if pi is None or m is None or S is None or g is None or li is None:
             return None, None, None, None
-        # NOTE p is the number of parameters, so k * d with the means,
-        # k * d * (d + 1) with the covariance matrix, and k - 1 with the priors
+
+        # NOTE p is the number of parameters
         p = (k * d) + (k * d * (d + 1) // 2) + (k - 1)
         bic = p * np.log(n) - 2 * li
 
@@ -90,7 +91,3 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
             best_bic = bic
             best_results = (pi, m, S)
             best_k = k
-
-    likelihoods = np.array(likelihoods)
-    b = np.array(b)
-    return best_k, best_results, likelihoods, b
