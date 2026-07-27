169bee0dbb88c348ae9fe4686b453627f6c3f2f9
Mathieu7483 <mathieu.godalier@orange.fr>
2026-07-27 13:29:25 +0200

    Remove noqa comment from GaussianProcess constructor for clarity

--- unsupervised_learning/hyperparameter_tuning/1-gp.py
+++ unsupervised_learning/hyperparameter_tuning/1-gp.py
@@ -6,7 +6,7 @@ import numpy as np
 class GaussianProcess:
     """Represents a noiseless 1D Gaussian process."""
 
-    def __init__(self, X_init, Y_init, l=1, sigma_f=1):  # noqa: E741
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
         """
         Class constructor.
 
