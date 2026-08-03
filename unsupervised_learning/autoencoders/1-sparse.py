c4c0f4981248a5a3062dcb76bf3995a8758606f3
Mathieu7483 <mathieu.godalier@orange.fr>
2026-08-03 13:31:10 +0200

    Add function to create a sparse autoencoder model

---
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""Write a function that creates a sparse autoencoder"""
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
+    """Creates a sparse autoencoder
+    Args:
+        input_dims: integer containing the dimensions of the model input
+        hidden_layers: list containing the number of nodes for each hidden
+        layer
+            in the encoder, respectively
+        latent_dims: integer containing the dimensions of the latent space
+        lambtha: regularization parameter used for L1 regularization on the
+        encoded output
+    Returns: encoder, decoder, auto
+        encoder is the encoder model
+        decoder is the decoder model
+        auto is the full autoencoder model
+    """
+
+    # Encoder
+    inputs = keras.Input(shape=(input_dims,))
+    x = inputs
+    for units in hidden_layers:
+        x = keras.layers.Dense(units, activation='relu')(x)
+    latent = keras.layers.Dense(
+        latent_dims,
+        activation='relu',
+        activity_regularizer=keras.regularizers.l1(lambtha))(x)
+    encoder = keras.Model(inputs, latent, name='encoder')
+
+    # Decoder
+    latent_inputs = keras.Input(shape=(latent_dims,))
+    x = latent_inputs
+    for units in reversed(hidden_layers):
+        x = keras.layers.Dense(units, activation='relu')(x)
+    outputs = keras.layers.Dense(input_dims, activation='sigmoid')(x)
+    decoder = keras.Model(latent_inputs, outputs, name='decoder')
+
+    # Autoencoder
+    auto_input = inputs
+    encoded_output = encoder(auto_input)
+    decoded_output = decoder(encoded_output)
+    auto = keras.Model(auto_input, decoded_output, name='autoencoder')
+
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
