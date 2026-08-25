66fe359235b83ca6a6b68df61dcc33d500ffe955
Mathieu7483 <mathieu.godalier@orange.fr>
2026-08-25 12:39:04 +0200

    Refactor tf_idf function to streamline processing and remove unnecessary preprocessing steps

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -1,50 +1,22 @@
 #!/usr/bin/env python3
-"""write a function that creates a TF-IDF embedding matrix"""
-import re
-import string
+"""creates a TF-IDF embedding matrix"""
+from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 
 
 def tf_idf(sentences, vocab=None):
     """creates a TF-IDF embedding matrix from a list of sentences"""
-    # Preprocessing the sentences
-    processed_sentences = []
-    for sentence in sentences:
-        s = sentence.lower()
-        s = re.sub(r"\'s\b", "", s)
-        s = re.sub(f"[{re.escape(string.punctuation)}]", "", s)
-        processed_sentences.append(s.split())
+    if vocab is not None:
+        vectorizer = TfidfVectorizer(vocabulary=vocab)
+    else:
+        vectorizer = TfidfVectorizer()
 
-    # Build vocab if None
+    X = vectorizer.fit_transform(sentences)
+    E = X.toarray()
+    
     if vocab is None:
-        vocab = sorted(set(
-            word for s in processed_sentences for word in s
-        ))
+        F = np.array(vectorizer.get_feature_names_out())
+    else:
+        F = np.array(vocab)
 
-    # Initialize word_to_idx map and matrices
-    word_to_idx = {word: i for i, word in enumerate(vocab)}
-    tf = np.zeros((len(sentences), len(vocab)), dtype=float)
-    num_docs = len(sentences)
-
-    for i, s in enumerate(processed_sentences):
-        total_words = len(s)
-        if total_words > 0:
-            for word in s:
-                if word in word_to_idx:
-                    tf[i, word_to_idx[word]] += 1
-            # Term Frequency (TF)
-            tf[i] /= total_words
-
-    # Document Frequency (DF)
-    df = np.sum(tf > 0, axis=0)
-
-    # Inverse Document Frequency (IDF) avec lissage (+ 1)
-    with np.errstate(divide='ignore'):
-        # On calcule log(N / df) puis on ajoute 1 (formule lissée)
-        idf = np.log(num_docs / df) + 1.0
-        idf[np.isinf(idf) | np.isnan(idf)] = 0.0
-
-    # TF-IDF Embedding
-    E = tf * idf
-
-    return E, np.array(vocab)
+    return E, F
