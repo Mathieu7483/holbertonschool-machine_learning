2f9e98174e4e17cb6714cde8728ad354cf2d61cf
Mathieu7483 <mathieu.godalier@orange.fr>
2026-08-26 17:42:48 +0200

    Add word2vec model implementation to create embedding matrix from sentences

---
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -0,0 +1,22 @@
+#!usr/bin/env python3
+"""creates a word2vec embedding matrix"""
+from gensim.models import Word2Vec
+
+
+def word2vec_model(sentences, vector_size=100, min_count=5,
+                   window=5, negative=5, cbow=True, epochs=5,
+                   seed=0, workers=1):
+    """creates a word2vec embedding matrix from a list of sentences"""
+    sg = 0 if cbow else 1
+    model = Word2Vec(
+        sentences=sentences,
+        vector_size=vector_size,
+        window=window,
+        min_count=min_count,
+        negative=negative,
+        sg=sg,
+        epochs=epochs,
+        seed=seed,
+        workers=workers
+    )
+    return model
