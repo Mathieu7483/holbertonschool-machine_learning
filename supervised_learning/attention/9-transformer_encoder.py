1df2f7c218b39c52b20c44e8d5c2fd788d332cde
Mathieu7483 <mathieu.godalier@orange.fr>
2026-09-08 18:11:19 +0200

    Refactor Encoder class: remove redundant positional_encoding method and streamline initialization

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -3,7 +3,7 @@
 tensorflow.keras.layers.Layer to create
 the encoder for a transformer"""
 import tensorflow as tf
-import numpy as np
+positional_encoding = __import__('4-positional_encoding').positional_encoding
 EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
 
 
@@ -17,31 +17,16 @@ class Encoder(tf.keras.layers.Layer):
         self.N = N
         self.dm = dm
         self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
-        self.positional_encoding = self.positional_encoding(max_seq_len,
-                                                            dm)
-
-        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate)
-                       for _ in range(N)]
-
+        self.positional_encoding = positional_encoding(max_seq_len, dm)
+        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate) for _ in range(N)]
         self.dropout = tf.keras.layers.Dropout(drop_rate)
-
-    def positional_encoding(self, max_seq_len, dm):
-        """Function that calculates the positional encoding."""
-        pos_enc = np.zeros((max_seq_len, dm))
-        position = np.arange(0, max_seq_len)[:, np.newaxis]
-        div_term = np.exp(np.arange(0, dm, 2) * -(np.log(10000.0) / dm))
-        pos_enc[:, 0::2] = np.sin(position * div_term)
-        pos_enc[:, 1::2] = np.cos(position * div_term)
-        pos_enc = pos_enc[np.newaxis, ...]
-        return tf.cast(pos_enc, dtype=tf.float32)
 
     def call(self, x, training, mask):
         """Function that performs the forward pass."""
         seq_len = tf.shape(x)[1]
         x = self.embedding(x)
         x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
-        x += self.positional_encoding[:, :seq_len]
-
+        x += self.positional_encoding[:seq_len, :]
         x = self.dropout(x, training=training)
 
         for i in range(self.N):
